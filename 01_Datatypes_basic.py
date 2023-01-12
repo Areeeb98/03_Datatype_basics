26d2e9b027cac347430be1c00bfeef290c64aa62
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-12 23:07:45 +0530

    Add files via upload

---
+++ 01_Datatypes_basic.py
@@ -0,0 +1,64 @@
+"""
+Day: Thursday
+Date: 12th Jan 2023
+Topic: Datatypes_basics
+Time: 7PM 
+Author: Areeb Ahmed
+"""
+
+# print(type(4))
+# print(type(0))
+# print(type(-3))
+# print(type(232565674326788864787445766533254836684578))
+
+# print(type(4.5))
+# print(type(-4.5))
+# print(type(.5))
+# print(type(.1))
+# print(type(-3554.5645))
+
+# print('')
+# print("")
+# print('''''')
+# print("""""")
+
+# print('hi\n')
+# print('hey')
+
+# print('hi' , 'hey')
+# print('hi ' , 'hey')
+# print('' , 'hey')
+# print(' ' , ' hey')
+
+# a=4
+# print(str(a)+ 'b')
+# print('a'+'b')
+# print('a+b')
+# print("a+b")
+# print('''a+b''')
+# print("""a+b""")
+
+# print('4+b')
+# a=4
+# print(str(a)+ '+b')
+
+
+# print(type(''))
+# print(type(""))
+# print(type(''''''))
+# print(type(""""""))
+
+# print(type('4'))
+# print(type("7.8"))
+# print(type('''hi'''))
+# print(type("""3.6.10"""))
+
+# print(5>7)
+# print(5<7)
+# print(int(5>7))
+# print(int(5<7))
+# print(float(5<7))
+# print(float(5>7))
+
+# print(type(True))
+# print(type(False))
